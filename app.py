686149d9034b55ab0b42e75fdbd3520999a46b58
shubham yadav <shubham32yadav@gmail.com>
2022-05-13 19:37:09 +0530

    created templates, forms, and functions for user login, registration

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import render_template
-
 from config import app
+from flask import redirect, render_template, url_for
+from forms import LoginForm, RegistrationForm
 
 
 @app.route("/")
 def entry_point():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (
+            form.email.data == "admin@bestpeers.com"
+            and form.password.data == "password"
+        ):
+            return redirect(url_for("/"))
+        else:
+            return "not validated"
+    return render_template("login.html", title="Login", form=form)
 
 
 @app.route("/about")
---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
